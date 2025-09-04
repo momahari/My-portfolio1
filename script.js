47a7c5de7a73ddf927d0b4277a4a304498d9d38a
momahari <165669237+momahari@users.noreply.github.com>
2025-09-04 15:52:11 +0100

    First Portfolio
    Inial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function togglemenu(){
+    const menu = document.querySelector(".hum-menu-links")
+    const humb = document.querySelector(".hum-icon")
+
+    menu.classList.toggle('open')
+    humb.classList.toggle('open')
+}
